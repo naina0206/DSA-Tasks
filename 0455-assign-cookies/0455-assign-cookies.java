64b7c84f430dfcff674f70a23921ca913d0fbd6f
naina0206 <132363673+naina0206@users.noreply.github.com>
2024-10-20 22:33:28 +0530

    Time: 11 ms (40.98%), Space: 48.9 MB (9.64%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int r=0; int l=0;
+        while(l<g.length && r<s.length){
+            if(s[r]>=g[l]){
+                l++;
+            }
+            r++;
+        }
+        return l;
+    }
+}
